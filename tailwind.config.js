58aa3618ceb1e02942f86a8c7263b3de7cf73ca1
Aziz <113197790+haquema@users.noreply.github.com>
2024-11-07 21:41:37 +0000

    install nextui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
+import { nextui } from "@nextui-org/react";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,5 +16,8 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [
+    nextui(),
+  ],
 };
